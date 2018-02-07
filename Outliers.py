984d56ae58709d559738658410a4ffba59b9e022
Tyler Will <twill4398@gmail.com>
2018-02-07 15:28:39 -0500

    New soln to 5
    Different approach does not time out

--- Outliers.py
+++ Outliers.py
@@ -1,23 +1,17 @@
-#NOT FINISHED
-#TIMES OUT ON 2 CASES
+# Parse Inputs 
+first_line = [int(x) for x in input().split()]
+N = first_line[0]
+K = first_line[1]
 
+# Sort the available bins 
+k_vals = sorted([int(x) for x in input().split(" ")])
 
-first_line = input()
-first_line = first_line.split(" ")
-N = int(first_line[0])
-K = int(first_line[1])
-line = input()
-line = line.split(" ")
-max_shift = 0
-i = 0
-while i <= int(N-1):
-    if i not in line:
-        min_dist = 1000000000000
-        for value in line:
-            dist = abs(int(value) - i)
-            if dist < min_dist:
-                min_dist = dist
-        if min_dist > max_shift:
-            max_shift = min_dist
-        i += 1
-print(max_shift)
+# Make a list of the differences between any middle bins
+differences = [int((j-i)/2) for i, j in zip(k_vals[:-1], k_vals[1:])]
+# Account for any ID being before the first bin 
+# Account for any ID being in between any available bins 
+# Account for any ID being past the last bin 
+differences = [k_vals[0]] + differences + [N - 1 - k_vals[-1]]
+
+# Max of these is the furthest possible distance 
+print(max(differences))
